32516a6819daff518dc2ce169cf07419c60e97d1
Hideki J <aledinai@gmail.com>
2018-10-07 00:53:20 -0300

    Add fusion tables / markers test

--- src/App.js
+++ src/App.js
@@ -3,10 +3,38 @@ import './App.css';
 import Map from './Map'
 
 class App extends Component {
+  state = {
+    markers: []
+  }
+  componentWillMount() {
+    this.testFetch()
+  }
+  testFetch() {
+    const url = 'https://www.googleapis.com/fusiontables/v2/query?sql=SELECT*%20FROM%201EoMGaJVXfDZdNy8YLMXdVpVaxM2rdurHVgY4X10P&key=AIzaSyAszEoz4HsD1TwV_9pZYzHJW3Fvd158C_M';
+    fetch(url)
+      .then(resp => resp.json())
+      .then(data => {
+        let markers = [];
+        data.rows.map((item) => {
+          const lat = item[3];
+          const lng = item[4];
+          return markers.push({
+            lat: lat,
+            lng: lng
+          })
+        })
+        return markers
+      }).then(resp => {
+        console.log(resp)
+        this.setState({
+          markers: resp
+        })
+      }).catch(err => console.log(err))
+  }
   render() {
     return (
       <div className="App">
-        <Map />
+        <Map markers={this.state.markers} />
       </div>
     );
   }
--- src/Map.js
+++ src/Map.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
-const MyMapComponent = withScriptjs(withGoogleMap((props) =>
-  <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
-  >
-    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
-  </GoogleMap>
-))
+const MyMapComponent = withScriptjs(withGoogleMap((props) => {
+  const markers = props.markers.map((marker, index) => (
+    <Marker
+      key={index}
+      position={{ lat: marker.lat, lng: marker.lng }}
+    />
+  ))
+  return (
+    <GoogleMap
+      defaultZoom={2}
+      defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    >
+      {props.isMarkerShown && markers}
+    </GoogleMap>
+  )
+}))
 
 class Map extends Component {
   render() {
     return (
       <MyMapComponent
         isMarkerShown
+        markers={this.props.markers}
         googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAszEoz4HsD1TwV_9pZYzHJW3Fvd158C_M"
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `100vh` }} />}
