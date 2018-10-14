a2230e353c7f2941a495359ad5097d3a6c59d260
Hideki J <aledinai@gmail.com>
2018-10-14 20:29:43 -0300

    Updates

---
+++ precache-manifest.59461376ba231f60aa9d8ba0b558c05f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5578166a92c66be80ea8e3d4852a5446",
+    "url": "/static/media/menu_close.5578166a.svg"
+  },
+  {
+    "revision": "af2574267dd950d9f2e66d7f8edb96b4",
+    "url": "/static/media/menu_burger.af257426.svg"
+  },
+  {
+    "revision": "6e02891bf0e2fa206806ce40e58d8958",
+    "url": "/static/media/logo.6e02891b.svg"
+  },
+  {
+    "revision": "2fc12074c012f123bddbcb1b2dcca6c5",
+    "url": "/static/media/error.2fc12074.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4d7bc7bb4c3047bca895",
+    "url": "/static/js/main.4d7bc7bb.chunk.js"
+  },
+  {
+    "revision": "5a3d82827c2c634445a1",
+    "url": "/static/js/1.5a3d8282.chunk.js"
+  },
+  {
+    "revision": "4d7bc7bb4c3047bca895",
+    "url": "/static/css/main.4c6f10cd.chunk.css"
+  },
+  {
+    "revision": "663a9db1193f6fec4d14ef44c1cc3a98",
+    "url": "/index.html"
+  }
+];
